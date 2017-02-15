11426c94bf704fa990928e7de596f585b418f944
yueyue <1263375728@qq.com>
2017-02-15 21:45:45 +0800

    heart myself

--- onlineUpdateClientHelp/Form1.Designer.cs
+++ onlineUpdateClientHelp/Form1.Designer.cs
@@ -28,9 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(415, 290);
+            this.Name = "Form1";
+            this.Text = "在线升级客户端帮助";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- onlineUpdateClientHelp/Form1.cs
+++ onlineUpdateClientHelp/Form1.cs
@@ -4,7 +4,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +15,79 @@ namespace onlineUpdateClientHelp
 {
     public partial class Form1 : Form
     {
+        //心跳发送间隔
+        public int heart = 30000;
+        public DateTime lastTime;
+        //用来发送
+        Thread tdSend;
+        Socket clientSocket;
+
+        public class user
+        {
+            public static int bufferSize = 1024;
+            public byte[] buffer = new byte[bufferSize];
+            public Socket workSocket;
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //连接服务器
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            EndPoint serverEP = new IPEndPoint(ip, 5555);
+        
+            //跟服务器相连的套接字
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clientSocket.Connect(serverEP);
+            //接收数据
+            user client = new user();
+            client.workSocket = clientSocket;
+            clientSocket.BeginReceive(client.buffer, 0, user.bufferSize, SocketFlags.None, new AsyncCallback(ReceiveCallback), client);
+
+            tdSend = new Thread(new ThreadStart(SendContinue));
+            tdSend.Start();
+           
+        }
+
+        public void SendContinue()
+        {
+            while(true)
+            {
+                //发送信号
+                byte[] buffer = Encoding.Default.GetBytes("1111111111");
+                clientSocket.BeginSend(buffer, 0, buffer.Length, SocketFlags.None,
+                    new AsyncCallback(SendCallback), clientSocket);
+                Thread.Sleep(heart);
+            }          
+        }
+        public void SendCallback(IAsyncResult ar)
+        {
+            Socket SocketEnd = (Socket)ar.AsyncState;
+            int SendBufferSize = SocketEnd.EndSend(ar);
+        }
+        public void ReceiveCallback(IAsyncResult ar)
+        {
+            user client = (user)ar.AsyncState;
+            try
+            {
+                int bytelength = client.workSocket.EndReceive(ar);
+                string msg = Encoding.Default.GetString(client.buffer, 0, bytelength);
+                if (msg == "successful")
+                {
+                    client.workSocket.BeginReceive(client.buffer, 0, user.bufferSize,
+                        SocketFlags.None, new AsyncCallback(ReceiveCallback), client);
+                    MessageBox.Show("successful");
+                    TimeSpan tn = DateTime.Now - lastTime;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("服务器关闭");
+            }
         }
     }
 }
